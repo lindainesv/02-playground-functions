ff5dfeff011a8617fc9aabcaeb675dc0910cc512
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-27 17:33:57 -0300

    formatação

--- challenges.js
+++ challenges.js
@@ -4,11 +4,11 @@
 // tem sol e nao tenho dinheiro > nao vou sair (false)
 // nao tem sol e nao tenho dinheiro > nao vou sair (false)
 
-// const condicao1 = 'sol';  
+// const condicao1 = 'sol';
 // const condicao2 = 'dinheiro';  >>> não são usados
 
-function compareTrue(condicao1, condicao2){
-  if (condicao1 === true && condicao2 ===  true) {
+function compareTrue(condicao1, condicao2) {
+  if (condicao1 === true && condicao2 === true) {
     return true;
   } else {
     return false;
@@ -17,20 +17,16 @@ function compareTrue(condicao1, condicao2){
 
 // console.log(compareTrue(true, false));
 
-
 // Requisito 2
-function calcArea (base, height) {
-return (base * height) / 2
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
-
-
 
 // Requisito 3
 // 3 - Crie uma função que divida uma frase de acordo com a quantidade de palavras
-//a função recebe como parametro uma string 
-//retornar um array com as palavras separadas por virgula 
+//a função recebe como parametro uma string
+//retornar um array com as palavras separadas por virgula
 
-function splitSentence (string) {
-	return splitSentence.split('');
+function splitSentence(string) {
+  return splitSentence.split('');
 }
-
