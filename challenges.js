e6fb3b3888f7be11ad75207d60481d65f7348d4b
lindainesv <lindainesvicente@hotmail.com.com>
2022-10-27 16:52:42 -0300

    correção requisito 1

--- challenges.js
+++ challenges.js
@@ -4,17 +4,43 @@
 // tem sol e nao tenho dinheiro > nao vou sair (false)
 // nao tem sol e nao tenho dinheiro > nao vou sair (false)
 
+// const condicao1 = 'sol';  
+// const condicao2 = 'dinheiro';  >>> não são usados
 
-const condicao1 = 'sol';
-const condicao2 =  'dinheiro';
-
-function compareTrue() {
-	if (condicao1 === "sol" && condicao2 === "dinheiro") {
-		console.log("Eba! Vou sair");
-	} else{
-		console.log("Poxa não vou sair #sad");
-	}
+function compareTrue(condicao1, condicao2){
+  if (condicao1 === true && condicao2 ===  true) {
+    return 'Eba! Vou sair';
+  } else {
+    return 'Poxa não vou sair #sad';
+  }
 }
 
+console.log(compareTrue(true, false));
 
+
+// Requisito 2
+
+
+
+
+
+// Requisito 3
+// 3 - Crie uma função que divida uma frase de acordo com a quantidade de palavras
+//a função recebe como parametro uma string 
+//retornar um array com as palavras separadas por virgula 
+
+
+
+
+let string = ['go', 'Trybe', 'foguete'];
+let splitSentence = string[1] 
+
+function splitSentence (string) {
+	splitSentence
+}
+
+return 
+ 
+
+console.log(splitSentence);
 
